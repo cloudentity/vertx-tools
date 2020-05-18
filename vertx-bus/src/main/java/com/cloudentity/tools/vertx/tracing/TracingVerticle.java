62784ae72808791ef1a369e25a07430130e97652
Marcin Nikliborc <mnikliborc@cloudentity.com>
2020-05-18 16:04:31 +0200

    ComponentVerticle does not print stacktrace when TracingVerticle not available

--- vertx-bus/src/main/java/com/cloudentity/tools/vertx/tracing/TracingVerticle.java
+++ vertx-bus/src/main/java/com/cloudentity/tools/vertx/tracing/TracingVerticle.java
@@ -2,10 +2,10 @@ package com.cloudentity.tools.vertx.tracing;
 
 import com.cloudentity.tools.vertx.bus.VertxEndpointClient;
 import com.cloudentity.tools.vertx.bus.ServiceVerticle;
-import com.cloudentity.tools.vertx.bus.VertxEndpointClient;
 import com.cloudentity.tools.vertx.conf.ConfService;
 import com.cloudentity.tools.vertx.tracing.internals.JaegerTracing;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,14 +15,21 @@ public class TracingVerticle extends ServiceVerticle implements TracingService {
   private ConfService confService;
   private TracingManager tracing;
 
+  private static boolean tracingEnabled;
+
   @Override
   public void start(Future<Void> start) {
     confService = VertxEndpointClient.make(vertx, ConfService.class);
     toFuture(super::start).compose(x -> JaegerTracing.getTracingConfiguration(confService)
       .compose(t -> {
         this.tracing = t;
+        tracingEnabled = true;
         return Future.<Void>succeededFuture();
       })).setHandler(start);
+  }
+
+  public static boolean isTracingEnabled(Vertx vertx) {
+    return tracingEnabled;
   }
 
   @Override
